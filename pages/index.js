440b831e24686fb67d14d9f2c260a2a15b065687
QauT <77263021+QauT@users.noreply.github.com>
2023-04-26 16:59:07 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,81 @@
-import Link from 'next/link';
-import { getPosts } from '../utils/mdx-utils';
-
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import Layout, { GradientBackground } from '../components/Layout';
-import ArrowIcon from '../components/ArrowIcon';
-import { getGlobalData } from '../utils/global-data';
-import SEO from '../components/SEO';
-
-export default function Index({ posts, globalData }) {
-  return (
-    <Layout>
-      <SEO title={globalData.name} description={globalData.blogTitle} />
-      <Header name={globalData.name} />
-      <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
-          {globalData.blogTitle}
-        </h1>
-        <ul className="w-full">
-          {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            >
-              <Link
-                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-                href={`/posts/[slug]`}
-              >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.data.date && (
-                    <p className="uppercase mb-3 font-bold opacity-60">
-                      {post.data.date}
-                    </p>
-                  )}
-                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                  {post.data.description && (
-                    <p className="mt-3 text-lg opacity-60">
-                      {post.data.description}
-                    </p>
-                  )}
-                  <ArrowIcon className="mt-4" />
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
-      <Footer copyrightText={globalData.footerText} />
-      <GradientBackground
-        variant="large"
-        className="fixed top-20 opacity-40 dark:opacity-60"
-      />
-      <GradientBackground
-        variant="small"
-        className="absolute bottom-0 opacity-20 dark:opacity-10"
-      />
-    </Layout>
-  );
-}
-
-export function getStaticProps() {
-  const posts = getPosts();
-  const globalData = getGlobalData();
-
-  return { props: { posts, globalData } };
-}
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Terms of Service</title>
+    <style>
+      /* Add your custom CSS styles here */
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f2f2f2;
+        color: #333;
+        margin: 0;
+        padding: 0;
+      }
+      
+      h1 {
+        text-align: center;
+        margin-top: 50px;
+        font-size: 36px;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+      }
+      
+      h2 {
+        margin-top: 40px;
+        font-size: 24px;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        color: #666;
+      }
+      
+      ul {
+        list-style-type: disc;
+        margin-left: 20px;
+        margin-bottom: 20px;
+      }
+      
+      p {
+        margin-bottom: 20px;
+        line-height: 1.5;
+      }
+      
+      .container {
+        max-width: 800px;
+        margin: 0 auto;
+        padding: 50px;
+        background-color: #fff;
+        box-shadow: 0 0 10px rgba(0,0,0,0.1);
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Terms of Service</h1>
+      <h2>1. Introduction</h2>
+      <p>These terms and conditions govern your use of the Jail bot on Discord.</p>
+      
+      <h2>2. License</h2>
+      <p>You are hereby granted a non-exclusive, non-transferable license to use the Jail bot.</p>
+      
+      <h2>3. Prohibited Use</h2>
+      <p>You may not use the Jail bot for any illegal or unauthorized purpose.</p>
+      <ul>
+        <li>You may not use the Jail bot to harass, abuse, or harm others.</li>
+        <li>You may not use the Jail bot to promote or engage in hate speech, discrimination, or violence.</li>
+        <li>You may not use the Jail bot to spam or send unsolicited messages.</li>
+      </ul>
+      
+      <h2>4. Termination</h2>
+      <p>We reserve the right to terminate your access to the Jail bot at any time, without notice.</p>
+      
+      <h2>5. Disclaimer</h2>
+      <p>The Jail bot is provided "as is" and we make no representations or warranties of any kind, express or implied.</p>
+      
+      <h2>6. Limitation of Liability</h2>
+      <p>We shall not be liable for any direct, indirect, incidental, special, or consequential damages arising out of or in connection with the use of the Jail bot.</p>
+      
+      <h2>7. Governing Law</h2>
+      <p>These terms and conditions shall be governed by and construed in accordance with the laws of [your jurisdiction].</p>
+    </div>
+  </body>
+</html>
